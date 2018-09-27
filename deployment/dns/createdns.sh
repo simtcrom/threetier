2bc90fcf7f1edd71eae967bb601139a616deecb7
simtcrom <simon.george@yahoo.com>
2018-09-27 16:56:30 +0530

    cgh

--- deployment/dns/createdns.sh
+++ deployment/dns/createdns.sh
@@ -1,4 +1,4 @@
 sleep 30
 LBNAME=`/usr/local/bin/kubectl describe service frontend|grep Ingress|awk '{print $NF}'`
 sed -i "s/FELBNAME/$LBNAME/g" dns.json
-aws route53 change-resource-record-sets --hosted-zone-id Z2VPD3S1OO9U30 --change-batch file://dns.json
+aws route53 change-resource-record-sets --hosted-zone-id Z3VT5R4V1WQDNS --change-batch file://dns.json
